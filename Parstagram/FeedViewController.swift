2226d02e99b0416d3d90f605e03d3487ff2e830b
Scott <agentwinburn1@gmail.com>
2020-10-29 23:21:41 +0800

    added: logout, saved comments

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -22,7 +22,6 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         tableView.delegate = self
         tableView.dataSource = self
         
-        tableView.reloadData()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -62,6 +61,32 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let post = posts[indexPath.row]
+        
+        let comment = PFObject(className: "Comments")
+        comment["text"] = "random comment"
+        comment["post"] = post
+        comment["author"] = PFUser.current()!
+        
+        post.add(comment, forKey: "comments")
+        
+        post.saveInBackground { (success, error) in
+            if success {
+                print("Comment saved")
+            } else {
+                print("Error saving comment")
+            }
+        }
+    }
+    
+    @IBAction func onLogoutButton(_ sender: UIBarButtonItem) {
+        PFUser.logOut()
+        dismiss(animated: true, completion: nil)
+        present(LoginViewController(), animated: true, completion: nil)
+    }
+    
 
     /*
     // MARK: - Navigation
